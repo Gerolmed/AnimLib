b64a994c5910d14c770325429d31e764be3c939b
Gerolmed <werwolf420@yahoo.de>
2019-06-05 14:20:08 +0200

    Added al args constructor

--- src/main/java/de/gerolmed/animlib/api/utils/ItemNameTag.java
+++ src/main/java/de/gerolmed/animlib/api/utils/ItemNameTag.java
@@ -1,8 +1,10 @@
 package de.gerolmed.animlib.api.utils;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ItemNameTag {
     private String name;
 }
